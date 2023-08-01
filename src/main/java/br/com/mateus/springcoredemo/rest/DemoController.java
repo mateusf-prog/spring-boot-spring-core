aca8df5f9c9349cf4be63069022e1e8585bc8ed6
mateusf-prog <mateus102006@hotmail.com>
2023-08-01 09:49:20 -0300

    bean lifecycle methods

--- src/main/java/br/com/mateus/springcoredemo/rest/DemoController.java
+++ src/main/java/br/com/mateus/springcoredemo/rest/DemoController.java
@@ -11,26 +11,16 @@ public class DemoController {
 
     private Coach myCoach;
 
-    private Coach anotherCoach;
-
     // beans using prototype scope
     @Autowired
-    public DemoController(
-            @Qualifier("cricketCoach") Coach theCoach,
-            @Qualifier("cricketCoach") Coach theAnotherCoach) {
+    public DemoController(@Qualifier("cricketCoach") Coach theCoach) {
         System.out.println("In constructor: " + getClass().getSimpleName());
         myCoach = theCoach;
-        anotherCoach = theAnotherCoach;
     }
 
     @GetMapping("/dailyworkout")
     public String getDailyWorkout() {
         return myCoach.getDailyWorkout();
     }
-
-    // endpoint for check comparison of beans
-    @GetMapping("/check")
-    public String check() {
-        return "Comparing beans: myCoach == anotherCoach, " + (myCoach == anotherCoach);
-    }
 }
+
